549483582d68882633899aa2e5beebc1a9329da4
Maria <kononetovama@mail.ru>
2026-03-16 17:13:02 +0300

    Домашнее задание 2 Развернуто React-приложение с Vite Создано три компонента

---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+function Card({ children }) {
+  return (
+    <div style={{ border: '2px solid #ccc', padding: '20px', borderRadius: '10px' }}>
+      {children} 
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/UserInfo.jsx
@@ -0,0 +1,10 @@
+function UserInfo({ name, profession }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Профессия: {profession}</p>
+    </div>
+  );
+}
+
+export default UserInfo;
